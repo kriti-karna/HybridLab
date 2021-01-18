e73724556b9cee9552b4fc7d15f008158ac9d931
kriti-karna <kritikarna97@gmail.com>
2021-01-18 14:53:57 +0545

    Hybrid Assignment

---
+++ app/src/main/java/com/kriti/account/admin_pg.java
@@ -0,0 +1,72 @@
+package com.kriti.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class admin_pg extends AppCompatActivity {
+
+    ImageView insert, delete, update, display, back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_pg);
+
+        back = findViewById(R.id.back);
+        insert = findViewById(R.id.insert);
+        delete = findViewById(R.id.delete);
+        update = findViewById(R.id.update);
+        display = findViewById(R.id.display);
+
+        back.setClickable(true);
+        insert.setClickable(true);
+        delete.setClickable(true);
+        update.setClickable(true);
+        display.setClickable(true);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), insert_pg.class);
+                startActivity(i);
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), delete_pg.class);
+                startActivity(i);
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), update_pg.class);
+                startActivity(i);
+            }
+        });
+
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), display_pg.class);
+                startActivity(i);
+            }
+        });
+    }
+}
